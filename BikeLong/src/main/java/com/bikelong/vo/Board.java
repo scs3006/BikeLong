ccef7ca474db5a77807ca962f78d7d1b5acf0893
Administrator <Administrator@MSDN-SPECIAL>
2018-07-20 17:03:21 +0900

    Vo 패키지의 trailBoard => Board 변경

---
+++ BikeLong/src/main/java/com/bikelong/vo/Board.java
@@ -0,0 +1,58 @@
+package com.bikelong.vo;
+
+
+public class Board {
+
+	private int boardNo;
+	private String id;
+	private int locationNo;
+	private int category;
+	private String date;
+	private String title;
+	private String content;
+
+
+	public int getBoardNo() {
+		return boardNo;
+	}
+	public void setBoardNo(int boardNo) {
+		this.boardNo = boardNo;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getLocationNo() {
+		return locationNo;
+	}
+	public void setLocationNo(int locationNo) {
+		this.locationNo = locationNo;
+	}
+	public int getCategory() {
+		return category;
+	}
+	public void setCategory(int category) {
+		this.category = category;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+}
